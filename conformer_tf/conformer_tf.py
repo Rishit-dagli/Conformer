1491be74c8388ca53d8f4fc197785fa1417df5ae
Rishit Dagli <rishit.dagli@gmail.com>
2022-01-01 18:09:04 +0000

    create Attention layer

--- conformer_tf/conformer_tf.py
+++ conformer_tf/conformer_tf.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+import einops
+from einops import rearrange
+
 
 class Swish(tf.keras.layers.Layer):
     def __init__(self, **kwargs):
@@ -6,6 +9,7 @@ class Swish(tf.keras.layers.Layer):
 
     def call(self, inputs):
         return inputs * tf.sigmoid(inputs)
+
 
 class GLU(tf.keras.layers.Layer):
     def __init__(self, diim, **kwargs):
@@ -15,36 +19,108 @@ class GLU(tf.keras.layers.Layer):
     def call(self, inputs):
         out, gate = tf.split(inputs, 2, axis=self.dim)
 
+
 class DepthwiseLayer(tf.keras.layers.Layer):
     def __init__(self, chan_in, chan_out, kernel_size, padding, **kwargs):
         super(DepthwiseLayer, self).__init__(**kwargs)
         self.padding = padding
-        self.conv = tf.keras.layers.Conv1D(chan_in, chan_out, kernel_size, groups = chan_in)
-    
+        self.conv = tf.keras.layers.Conv1D(
+            chan_in, chan_out, kernel_size, groups=chan_in
+        )
+
     def call(self, inputs):
         pad_list = []
         for _ in range(inputs.shape.rank - 1):
             pad_list.append([0, 0])
         pad_list.append(list(self.padding))
         inputs = tf.pad(inputs, pad_list)
+
         return self.conv(inputs)
+
 
 class Scale(tf.keras.layers.Layer):
     def __init__(self, scale, fn, **kwargs):
         super(Scale, self).__init__(**kwargs)
         self.scale = scale
         self.fn = fn
-    
+
     def call(self, inputs, **kwargs):
         return self.fn(inputs, **kwargs) * self.scale
+
 
 class PreNorm(tf.keras.layers.Layer):
     def __init__(self, dim, fn, **kwargs):
         super(PreNorm, self).__init__(**kwargs)
         self.norm = tf.keras.layers.LayerNormalization(axis=-1)
         self.fn = fn
-    
+
     def call(self, inputs, **kwargs):
         inputs = self.norm(inputs)
         return self.fn(inputs, **kwargs)
 
+
+class Attention(tf.keras.layers.Layer):
+    def __init__(
+        self, dim, heads=8, dim_head=64, dropout=0.0, max_pos_emb=512, **kwargs
+    ):
+        super(Attention).__init__(**kwargs)
+        inner_dim = dim_head * heads
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+
+        self.to_q = tf.keras.layers.Dense(inner_dim, input_dim=dim, use_bias=False)
+        self.to_kv = tf.keras.layers.Dense(inner_dim * 2, input_dim=dim, use_bias=False)
+        self.to_out = tf.keras.layers.Dense(dim, input_dim=inner_dim)
+
+        self.max_pos_emb = max_pos_emb
+        self.rel_pos_emb = tf.keras.layers.Embedding(2 * max_pos_emb + 1, dim_head)
+
+        self.dropout = tf.keras.layers.Dropout(dropout)
+
+    def call(self, inputs, context=None, mask=None, context_mask=None):
+        n = inputs.shape[-2]
+        heads = self.heads
+        max_pos_emb = self.max_pos_emb
+        if context is None:
+            has_context = False
+            context = inputs
+        else:
+            has_context = True
+
+        q, k, v = tf.split((self.to_q(x), *self.to_kv(context)), 2, axis=-1)
+        q, k, v = map(lambda t: rearrange(t, "b n (h d) -> b h n d", h=h), (q, k, v))
+        dots = tf.einsum("b h i d, b h j d -> b h i j", q, k) * self.scale
+
+        seq = tf.range(n)
+        dist = rearrange(seq, "i -> i ()") - rearrange(seq, "j -> () j")
+        dist = (
+            tf.clip_by_value(
+                dist, clip_value_min=-max_pos_emb, clip_value_max=max_pos_emb
+            )
+            + max_pos_emb
+        )
+        rel_pos_emb = self.rel_pos_emb(dist)
+        pos_attn = tf.einsum("b h n d, n r d -> b h n r", q, rel_pos_emb) * self.scale
+        dots = dots + pos_attn
+
+        if mask is not None or context_mask is not None:
+            if mask is not None:
+                mask = torch.ones(*inputs.shape[:2])
+            if not has_context:
+                if context_mask is None:
+                    context_mask = mask
+            else:
+                if context_mask is None:
+                    context_mask = torch.ones(*context.shape[:2])
+            mask_value = -tf.experimental.numpy.finfo(dots.dtype).max
+            mask = rearrange(mask, "b i -> b () i ()") * rearrange(
+                context_mask, "b j -> b () () j"
+            )
+            dots = tf.where(mask, mask_value, dots)
+
+        attn = tf.nn.softmax(dots, axis=-1)
+
+        out = tf.einsum('b h i j, b h j d -> b h i d', attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        out = self.to_out(out)
+        return self.dropout(out)
