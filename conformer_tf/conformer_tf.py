bafd637e3ab8ae5a8c110f6aa5d5ef1671617839
Rishit Dagli <rishit.dagli@gmail.com>
2022-01-01 16:56:21 +0000

    Create GLU activation layer

--- conformer_tf/conformer_tf.py
+++ conformer_tf/conformer_tf.py
@@ -7,6 +7,13 @@ class Swish(tf.keras.layers.Layer):
     def call(self, inputs):
         return inputs * tf.sigmoid(inputs)
 
+class GLU(tf.keras.layers.Layer):
+    def __init__(self, diim, **kwargs):
+        super(GLU, self).__init__(**kwargs)
+        self.dim = dim
+
+    def call(self, inputs):
+        out, gate = tf.split(inputs, 2, axis=self.dim)
 
 
 
